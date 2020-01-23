57d4caf7dd4a7ca1d069df3101635de367aff2bf
Ning Zhou <ning@webtools.co.nz=>
2020-01-24 12:44:18 +1300

    second push

--- src/App.js
+++ src/App.js
@@ -1,15 +1,64 @@
-import React from 'react';
+import React from "react";
+import { Switch, Route } from "react-router-dom";
+import "./App.css";
 
-import './App.css';
+import HomePage from "./pages/homepage/homepage.component";
 
-import HomePage from './pages/homepage/homepage.component'
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE </h1>
+  </div>
+);
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/hats" component={HatsPage} />
+      </Switch>
     </div>
   );
 }
+
+// const HomePage = (props) => {
+//   return (
+//     <div>
+//       <Link to="/topics">Topic</Link>
+//       <button onClick={()=>props.history.push('/topics')}>Topics </button>
+//       <h1>Home Page</h1>
+//     </div>
+//   );
+// };
+
+// const TopicList = (props) => {
+//   console.log(props);
+//   return (
+//     <div>
+//       <h1>Topic list page</h1>
+//     </div>
+//   );
+// };
+
+// const TopicDetail = props => {
+//   console.log(props);
+//   return (
+//     <div>
+//       <h1>Topic detail page: {props.match.params.topicId}</h1>
+//     </div>
+//   );
+// };
+
+// const App = () => {
+//   return (
+//     <div>
+//       <Switch>
+//         <Route path="/topic/:topicId" component={TopicDetail} />
+//         <Route path="/topics" component={TopicList} />
+//         <Route path="/" component={HomePage} />
+//       </Switch>
+//     </div>
+//   );
+// };
 
 export default App;
