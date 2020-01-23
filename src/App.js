684c64a1d009195faa6ad156a32c1ff0ceecdb31
Ning Zhou <ning@webtools.co.nz=>
2020-01-23 16:48:25 +1300

    59

--- src/App.js
+++ src/App.js
@@ -1,39 +1,15 @@
 import React from 'react';
 
-import logo from './logo.svg';
 import './App.css';
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      meaningOfLife: 47 + this.props.increment
-    }
-  }
+import HomePage from './homepage.component'
 
-  handleClick = () => {
-    this.setState((prevState, prevProps) => {
-      return {meaningOfLife: prevState.meaningOfLife + prevProps.increment}
-    }, 
-    ()=>console.log(this.state.meaningOfLife)
-    )
-  }
-
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} alt="logo" className="App-logo" />
-          <p>
-            {this.state.meaningOfLife}
-          </p>
-          <button onClick={this.handleClick}>
-            Update state
-          </button>
-        </header>
-      </div>
-    )
-  }
+function App() {
+  return (
+    <div>
+      <HomePage />
+    </div>
+  );
 }
 
 export default App;
